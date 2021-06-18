6e39e40ed5947cbb01fb108e6f89f04875a55391
ayadiRahmat <ayadi.rhmd@gmail.com>
2021-06-18 11:24:42 +0700

    upload projek vue

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# shopvue
-Projek aplikasi Vue sebagai bahan belajar Full-Stack Laravel VueJS (Toko Online).
+# shop
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="name">
+    <HeaderShop />
+    <HeroShop />
+    <WomanShop />
+    <RelatedShop />
+    <InstaShop />
+    <PartnerShop />
+    <FooterShop />
+  </div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShop from '@/components/HeaderShop.vue';
+import HeroShop from '@/components/HeroShop.vue';
+import WomanShop from '@/components/WomanShop.vue';
+import InstaShop from '@/components/InstaShop.vue';
+import PartnerShop from '@/components/PartnerShop.vue';
+import RelatedShop from '@/components/RelatedShop.vue';
+import FooterShop from '@/components/FooterShop.vue';
+
+export default {
+  name: 'home',
+  components: {
+    HeaderShop,
+    HeroShop,
+    WomanShop,
+    InstaShop,
+    PartnerShop,
+    RelatedShop,
+    FooterShop
+    
+  }
+};
+</script>
